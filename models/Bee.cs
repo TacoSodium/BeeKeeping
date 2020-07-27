3645cd762056c22da05d9c273045e229705c35b0
Coryn Tyrrell <103038198@student.swin.edu.au>
2020-07-27 15:07:28 +1000

    bee class

---
+++ models/Bee.cs
@@ -0,0 +1,14 @@
+namespace models
+{
+    public class Bee
+    {
+        public string Name;
+        public int Size;
+
+        public Bee(string name, int size)
+        {
+            this.Name = name;
+            this.Size = size;
+        }
+    }
+}
