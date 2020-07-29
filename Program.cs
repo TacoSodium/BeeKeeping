70399108c6eee570bb172df7a572f57b20903a7a
Coryn Tyrrell <103038198@student.swin.edu.au>
2020-07-29 11:18:25 +1000

    AddBees method

--- Program.cs
+++ Program.cs
@@ -7,14 +7,6 @@ namespace models
     {
         static void Main(string[] args)
         {
-            Bee beeJohn = new Bee("John", 3.2);
-            Bee beePaul = new Bee("Paul", 2.7);
-            Bee beeGeorge = new Bee("George", 1.1);
-            Bee beeRingo = new Bee("Ringo", 2.0);
-            Bee beeKurt = new Bee("Kurt", 2.3);
-            Bee beeDave = new Bee("Dave", 7.4);
-            Bee beeKrist = new Bee("Krist", 1.5);
-
             Menu menu = new Menu();
 
             menu.Start();
